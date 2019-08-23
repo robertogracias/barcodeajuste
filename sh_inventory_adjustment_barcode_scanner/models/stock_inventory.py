5fcf574e227468c9d3870ff74ffbe54d863dc16e
Roberto Leonel Gracias <roberto.gracias@strategi-k.com>
2019-08-23 09:45:02 -0600

    update para presentar el ultimo producto

--- sh_inventory_adjustment_barcode_scanner/models/stock_inventory.py
+++ sh_inventory_adjustment_barcode_scanner/models/stock_inventory.py
@@ -6,56 +6,57 @@ from odoo.exceptions import UserError
 
 class StockInventory(models.Model):
     _inherit='stock.inventory'
- 
+    product_id=fields.Many2one(comodel_name='product.product', string='ULTIMO PRODUCTO ESCANEADO')
+    cantidad=fields.Integer("ULTIMA CANTIDAD LEIDA")
+
     @api.one
     def sh_on_barcode_scanned(self, barcode):
-                
+
         inventory_id =self.id
-        stock_inventory = self.env['stock.inventory'].search([('id', '=', inventory_id)],limit=1)       
-            
+        stock_inventory = self.env['stock.inventory'].search([('id', '=', inventory_id)],limit=1)
+
         if stock_inventory:
             product_id = self.env['product.product'].search([('barcode', '=', barcode)],limit=1)
-    
-            if product_id:                
 
-                location_id = stock_inventory.location_id                
+            if product_id:
+
+                location_id = stock_inventory.location_id
                 company = self.env.user.company_id
                 warehouse = self.env['stock.warehouse'].search([('company_id', '=', company.id)], limit=1)
-                
+
 #                if warehouse:
 #                    location_id = warehouse.lot_stock_id.id
 #                else:
 #                    raise UserError(_('You must define a warehouse for the company: %s.') % (company.name,))
-                                
-                if not stock_inventory.line_ids:    
+
+                if not stock_inventory.line_ids:
                     inventory_line_val = {
                             'display_name': product_id.name,
                             'product_id': product_id.id,
-                            'location_id':location_id,   
+                            'location_id':location_id,
                             'product_qty': 1,
                             'product_uom_id': product_id.product_tmpl_id.uom_id.id,
                             'inventory_id': stock_inventory.id
                     }
-                    
-                    stock_inventory.update({'line_ids': [(0, 0, inventory_line_val)]})
-                
+                    stock_inventory.update({'line_ids': [(0, 0, inventory_line_val)],'product_id':product_id.id,'cantidad':1})
+
                 else:
                     stock_picking_line = stock_inventory.line_ids.search([('product_id', '=', product_id.id),('inventory_id','=',inventory_id)], limit=1)
-                     
-                    if stock_picking_line:         
+                    cantidad=1
+                    if stock_picking_line:
                         stock_picking_line.product_qty += 1
-
-                    else :    
+                        cantidad=tock_picking_line.product_qty
+                    else :
                         inventory_line_val = {
                             'display_name': product_id.name,
                             'product_id': product_id.id,
-                            'location_id':location_id,   
+                            'location_id':location_id,
                             'product_qty': 1,
                             'product_uom_id': product_id.product_tmpl_id.uom_id.id,
                             'inventory_id': stock_inventory.id
                         }
-                        
-                        stock_inventory.update({'line_ids': [(0, 0, inventory_line_val)]})
-                        
+
+                        stock_inventory.update({'line_ids': [(0, 0, inventory_line_val)],'product_id':product_id.id,'cantidad':cantidad})
+
             else :
                 raise UserError('Product does not exist')
