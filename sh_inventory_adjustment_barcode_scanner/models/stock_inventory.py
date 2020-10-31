ce64cdd1a2d96108f415ec574ce734e6a5991455
Roberto Leonel Gracias <roberto.gracias@strategi-k.com>
2020-10-31 07:01:48 -0600

    Inventario lectura

--- sh_inventory_adjustment_barcode_scanner/models/stock_inventory.py
+++ sh_inventory_adjustment_barcode_scanner/models/stock_inventory.py
@@ -56,18 +56,58 @@ class mrp_process_production(models.Model):
     _inherit='mrp.production'
     recibido_lab=fields.Boolean('Recibido en laboratorio')
 
+class stock_paquete(models.Model):
+    _inherit='stock.quant.package'
+    inventory_id=fields.Many2one(comodel_name='stock.inventory', string='Invnetario')
+    inventory_lines=fields.One2many(comodel_name='stock.inventory.line',inverse_name='package_id',string="paquetes")
+    total=fields.Float("Total",compute='calcular_total',store=False)
+    
+    @api.multi
+    @api.depends('inventory_lines')
+    def calcular_total(self):
+        for r in self:
+            total=0.0
+            for p in r.inventory_lines:
+                total=total+p.product_qty
+            r.total=total
+        
+
 
 class StockInventory(models.Model):
     _inherit='stock.inventory'
     last_product_id=fields.Many2one(comodel_name='product.product', string='ULTIMO PRODUCTO ESCANEADO')
     last_cantidad=fields.Integer("ULTIMA CANTIDAD LEIDA")
+    nueva_caja=fields.Char("Nueva Caja")
+    caja_actual=fields.Char("Nueva Caja")
+    paquete_actual=fields.Many2one(comodel_name='stock.quant.package', string='Paquete Actual')
+    cantidad_actual=fields.Float(string="Cantidad en el paquete actual",compute='getcantidad_actual',store=False)
+    paquetes=fields.One2many(comodel_name='stock.quant.package',inverse_name='inventory_id',string="paquetes")
+    
+    @api.multi
+    @api.depends('paquete_actual')
+    def getcantidad_actual(self):
+        for r in self:
+            if r.paquete_actual:
+                r.cantidad_actual=r.paquete_actual.total
+    
+    @api.multi
+    def crear_paquete(self):
+        for r in self:
+            paquete=self.env['stock.quant.package'].search([('name','=',r.nueva_caja)])
+            if not paquete:
+                paquete=self.env['stock.quant.package'].create({'name':r.nueva_caja,'location_id':r.location_id.id,'inventory_id':r.id})
+                r.paquete_actual=paquete.id
+            else:
+                if paquete.inventory_id.id!=r.id:
+                    r.paquete_actual=None
+                    raise UserError('EL PAQUETE YA FUE CONTADO')
+                else:
+                    r.paquete_actual=paquete.id
 
     @api.one
     def sh_on_barcode_scanned(self, barcode):
-
         inventory_id =self.id
         stock_inventory = self.env['stock.inventory'].search([('id', '=', inventory_id)],limit=1)
-
         if stock_inventory:
             product_id = self.env['product.product'].search([('barcode', '=', barcode)],limit=1)
             if not product_id:
@@ -75,23 +115,20 @@ class StockInventory(models.Model):
                 if multi:
                     if multi.product_id:
                         product_id=self.env['product.product'].search([('id', '=', multi.product_id.id)],limit=1)
-
             if product_id:
-
                 location_id = stock_inventory.location_id
                 company = self.env.user.company_id
                 warehouse = self.env['stock.warehouse'].search([('company_id', '=', company.id)], limit=1)
-
 #                if warehouse:
 #                    location_id = warehouse.lot_stock_id.id
 #                else:
 #                    raise UserError(_('You must define a warehouse for the company: %s.') % (company.name,))
-
                 if not stock_inventory.line_ids:
                     inventory_line_val = {
                             'display_name': product_id.name,
                             'product_id': product_id.id,
                             'location_id':location_id,
+                            'package_id':stock_inventory.paquete_actual.id,
                             'product_qty': 1,
                             'product_uom_id': product_id.product_tmpl_id.uom_id.id,
                             'inventory_id': stock_inventory.id
@@ -101,7 +138,7 @@ class StockInventory(models.Model):
                     stock_inventory.update({'line_ids': [(0, 0, inventory_line_val)]})
 
                 else:
-                    stock_picking_line = stock_inventory.line_ids.search([('product_id', '=', product_id.id),('inventory_id','=',inventory_id)], limit=1)
+                    stock_picking_line = stock_inventory.line_ids.search([('product_id', '=', product_id.id),('inventory_id','=',inventory_id),('package_id','=',stock_inventory.paquete_actual.id)], limit=1)
                     cantidad=1
                     if stock_picking_line:
                         stock_picking_line.product_qty += 1
@@ -113,6 +150,7 @@ class StockInventory(models.Model):
                             'display_name': product_id.name,
                             'product_id': product_id.id,
                             'location_id':location_id,
+                            'package_id':stock_inventory.paquete_actual.id,
                             'product_qty': 1,
                             'product_uom_id': product_id.product_tmpl_id.uom_id.id,
                             'inventory_id': stock_inventory.id
